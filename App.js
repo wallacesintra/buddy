b02b78ccc7ff9ed3e10dec1af14c1d44f87841ab
wallacesintra <wallvxwallace962@gmail.com>
2023-12-07 14:28:23 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Sign_In from './src/screens/Sign_In';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Sign_In/>
     </View>
   );
 }
